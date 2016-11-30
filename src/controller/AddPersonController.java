54b6e9496404679a63ede0e3afc5990d128cdd0f
Viv <vivianmaryjanelai@gmail.com>
2016-11-29 19:46:53 -0700

    added base controller codes

---
+++ src/controller/AddPersonController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.BookingDao;
+import dao.LocationDao;
+import dao.PersonDao;
+import model.Booking;
+import model.Location;
+import model.Person;
+import util.DateUtil;
+
+@WebServlet("/AddPersonController")
+public class AddPersonController extends HttpServlet {
+	
+	/*
+	 * <form action="AddBookingController" method="post">
+	 *     Start Date: <input type="date" name="startDate"/> <br/>
+	 *     End Date: <input type="date" name="endDate"/> <br/>
+	 *     Location: <select name="location">
+	 *         <% for (Location location : LocationDao.getInstance().getAllLocations()) { %>
+	 *             <option value="<%=location.getId()%>"><%=location.getName()%></option> 
+	 *         <% } %>
+	 *     </select> <br/>
+	 *     Additional Persons (comma separated email list): <input type="text" name="persons"/>
+	 *     <input type="submit" value="submit"/>
+	 * </form>
+	 */
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+	}
+}
